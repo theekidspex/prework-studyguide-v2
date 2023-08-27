7b6b68464efcc4b67fd015bf1b6d9a81f34cc37e
theekidspex <theekidspex@gmail.com>
2023-08-27 11:06:58 -0700

    adding func. code

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x])
+    }
+}
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
 
-/*
+function selectTopic(){
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -16,4 +21,4 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
+}
