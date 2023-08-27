4b218625508cb4cbd4f3afcc52c6fee72da22f65
theekidspex <theekidspex@gmail.com>
2023-08-27 11:15:15 -0700

    fix

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,11 @@ function listTopics() {
 function selectTopic(){
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (topics === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (topics === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (topics === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
