762b3c6a05958e179431f1721da7c0516816d529
theekidspex <theekidspex@gmail.com>
2023-08-27 10:38:17 -0700

    code fix

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-/*
+
 var topic = "HTML";
 
 if (topic === 'HTML') {
@@ -16,7 +16,7 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
-*/
+
 
 
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
